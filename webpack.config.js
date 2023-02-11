41cc7c5889971ffb0eb82a6f13ddc5b4b231551b
David Hide <d.hide@live.co.uk>
2023-02-11 16:36:18 +0000

    Minify CSS

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = {
 	mode: 'development',
@@ -15,6 +17,7 @@ module.exports = {
 				{ from: 'src/images', to: 'images', globOptions: { ignore: ['*.js'] } }
 			],
 		}),
+		new MiniCssExtractPlugin()
 	],
 	output: {
 		filename: 'main.js',
@@ -25,8 +28,7 @@ module.exports = {
 			{
 				test: /\.s[ac]ss$/i,
 				use: [
-					// Creates `style` nodes from JS strings
-					"style-loader",
+					MiniCssExtractPlugin.loader,
 					// Translates CSS into CommonJS
 					"css-loader",
 					// Compiles Sass to CSS
@@ -38,6 +40,12 @@ module.exports = {
 				type: 'asset/resource',
 			},
 		],
+	},
+	optimization: {
+		minimizer: [
+			new CssMinimizerPlugin(),
+		],
+		minimize: true,
 	},
 	devServer: {
 		static: {
